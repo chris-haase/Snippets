cfc6d4932101f267ed6424974ad51bb7b0ea1be1
^Y^ <chris.haase@icetec.com>
2019-04-29 11:30:27 -0400

    Update take_stream.ex

--- Elixir/take_stream.ex
+++ Elixir/take_stream.ex
@@ -8,3 +8,8 @@ Enum.take(Stream.iterate(Date.utc_today, &Timex.shift(&1, days: -1)), 7)
 
 # implies Next N days
 Enum.take(Stream.iterate(Date.utc_today, &Timex.shift(&1, days: 1)), 7)              
+
+
+
+dates = Enum.take(Stream.iterate(Date.utc_today, &Timex.shift(&1, days: -1)), 7) 
+dates |> Enum.each( &function(&1))
